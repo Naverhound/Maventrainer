75c60165a933c32d9b381e9875d4d6aa143bdc12
Naverhound <39379474+Naverhound@users.noreply.github.com>
2020-05-21 17:11:44 -0600

    Diseño Beta chat
    Diseño beta para el chat con los clientes de lso personalizados o incluso como cliente.

--- resources/views/inc/head.blade.php
+++ resources/views/inc/head.blade.php
@@ -5,6 +5,5 @@
     <meta name="description" content="">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     @include('inc.styles')
-    
-    
+    @stack('Ccss')
 </head>
--- routes/web.php
+++ routes/web.php
@@ -12,7 +12,9 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
-
+Route::get('/chat', function () {
+   return view('public.chat'); 
+});
 
 Route::group(['prefix' => 'home' , 'middleware'=>'auth'], function () {
     Route::get('/', 'HomeController@index')->name('home');
